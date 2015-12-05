3e9490399203c456211cae2861e25480c4d0878d
tso <tso@teknik.io>
2015-12-05 09:09:39 -0500

    k

--- l.go
+++ l.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"github.com/lintianzhi/ignore"
-	"github.com/petermattis/linguist"
 	"io/ioutil"
 	"mime"
 	"os"
+	"path/filepath"
 	"strings"
+
+	"camlistore.org/pkg/magic"
+	"github.com/lintianzhi/ignore"
+	"github.com/petermattis/linguist"
 )
 
 func checkErr(err error) {
@@ -17,9 +21,10 @@ func checkErr(err error) {
 	}
 }
 
-var res map[string]string = make(map[string]string)
 var langs map[string]int64 = make(map[string]int64)
-var num_files int64 = 0
+var total_size int64 = 0
+var res map[string]int = make(map[string]int)
+var num_files int = 0
 var max_len int = 0
 
 func getLang(filename string) string {
@@ -38,11 +43,20 @@ func getLang(filename string) string {
 	parts := strings.Split(filename, ".")
 	ext := parts[len(parts)-1]
 	mimetype := mime.TypeByExtension("." + ext)
-	if mimetype == "" {
-		fmt.Printf("unknown ext: %s\nfilename: %s\n\n", ext, filename)
+	if mimetype != "" {
+		return mimetype
 	}
-	return mimetype
+
+	mimetyperedux := magic.MIMEType(contents)
+	if mimetyperedux != "" {
+		return mimetyperedux
+	}
+
+	//fmt.Fprintf(os.Stderr, "unknown ext: %s\nfilename: %s\n\n", ext, filename)
+	return "(unknown)"
 }
+
+var stderr *bufio.Writer = bufio.NewWriter(os.Stderr)
 
 func processDir(dirname string) {
 	cwd, err := os.Open(dirname)
@@ -51,6 +65,11 @@ func processDir(dirname string) {
 	checkErr(err)
 	checkErr(os.Chdir(dirname))
 	for _, file := range files {
+		if abs, err := filepath.Abs(file.Name()); err == nil {
+			fmt.Fprintf(stderr, "% 80s\r", " ")
+			fmt.Fprintf(stderr, "%s...\r", abs)
+			stderr.Flush()
+		}
 		if isIgnored(dirname + string(os.PathSeparator) + file.Name()) {
 			continue
 		}
@@ -62,9 +81,10 @@ func processDir(dirname string) {
 		} else if !linguist.IsVendored(file.Name()) {
 			//println(file.Name())
 			res1 := getLang(file.Name())
-			res[file.Name()] = res1
+			res[res1]++
 			langs[res1] += file.Size()
-			num_files += file.Size()
+			total_size += file.Size()
+			num_files++
 			if len(res1) > max_len {
 				max_len = len(res1)
 			}
@@ -93,8 +113,14 @@ func main() {
 		}
 	}
 	processDir(".")
-	fmtstr := fmt.Sprintf("%% %ds: %%04.4f%%%%\n", max_len)
+	fmt.Fprintf(stderr, "% 80s\r", " ")
+	fmt.Println()
+	fmtstr := fmt.Sprintf("%% %ds", max_len)
+	fmt.Printf(fmtstr, "Language")
+	fmt.Println(" (Size)  (Frequency)\n---")
+	fmtstr += ": %07.4f%% %07.4f%%\n"
 	for lang, num := range langs {
-		fmt.Printf(fmtstr, lang, (float64(num)/float64(num_files))*100.0)
+		fmt.Printf(fmtstr, lang, (float64(num)/float64(total_size))*100.0, (float64(res[lang])/float64(num_files))*100.0)
 	}
+	fmt.Printf("---\n%d languages detected in %d bytes of %d files\n", len(langs), total_size, num_files)
 }
